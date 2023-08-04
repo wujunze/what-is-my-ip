264eba81372897a4fc6418ae15280ea326ce5b48
liujin <liujin0215@hotmail.com>
2023-08-04 15:53:48 +0800

    move koa to src folder

--- api/index.ts
+++ api/index.ts
@@ -1,19 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import Router from "@koa/router";
-import Koa from "koa";
-
-function createApp() {
-    const dev = process.env.NODE_ENV !== 'production'
-
-    const server = new Koa({ proxy: !dev })
-    const router = new Router()
-
-    router.get('/', ctx => {
-        ctx.body = ctx.ip
-    })
-    server.use(router.routes())
-    return server
-}
+import { createApp } from "../src"
 
 function createHandler() {
     const app = createApp()
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import Router from "@koa/router";
+import Koa from "koa";
+
+export function createApp() {
+    const dev = process.env.NODE_ENV !== 'production'
+
+    const server = new Koa({ proxy: !dev })
+    const router = new Router()
+
+    router.get('/', ctx => {
+        ctx.body = ctx.ip
+    })
+    server.use(router.routes())
+    return server
+}
