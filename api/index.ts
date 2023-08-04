360be8a6b8e7384c903e25ab592d7c0189ab399c
liujin <liujin0215@hotmail.com>
2023-08-04 16:10:47 +0800

    remove src folder

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { createApp } from "../src"
+import { createApp } from "./koa"
 
 function createHandler() {
     const app = createApp()
